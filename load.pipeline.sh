afb23f36795a73920418411fc567f7a94a69032a
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-20 02:49:52 +0100

    Setting up first draft for k8s cluster provisioning with aws ec2 free tier

--- load.pipeline.sh
+++ load.pipeline.sh
@@ -4,9 +4,7 @@ export HORODATAGE=`date +%m-%d-%Y_%Hh-%Mmin-%Ssec`
 export OPS_HOME=$(pwd)
 export PROVISIONING_HOME=$(mktemp -d -t a-k8s-demo.${HORODATAGE}.provisioning-XXXXXXXXXX)
 
-
 export KUBERNETES_VERSION=${KUBERNETES_VERSION:-'1.17.4'}
-
 export K8S_PACKAGE_DWNLD_URI=https://github.com/kubernetes/kubernetes/releases/download/v${KUBERNETES_VERSION}/kubernetes.tar.gz
 export K8S_PACKAGE_FILENAME=$(echo $K8S_PACKAGE_DWNLD_URI|awk -F '/' '{print $NF}')
 
@@ -14,7 +12,8 @@ echo ""
 echo "Downloading [$K8S_PACKAGE_FILENAME] version [$KUBERNETES_VERSION] ..."
 echo ""
 
-wget $K8S_PACKAGE_DWNLD_URI
+# [-c] option to continue / resume downloads on network hiccups
+wget -c $K8S_PACKAGE_DWNLD_URI
 
 echo ""
 echo "Installing [$K8S_PACKAGE_FILENAME] version [$KUBERNETES_VERSION] to [$PROVISIONING_HOME] ..."
