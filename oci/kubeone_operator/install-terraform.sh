18fc3dbc6eb0052aaa8bd9f8473ed1886b1b0754
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-23 06:36:25 +0100

    Resuming work on [k8s-provisioning]

--- oci/kubeone_operator/install-terraform.sh
+++ oci/kubeone_operator/install-terraform.sh
@@ -46,8 +46,9 @@ gpg --full-generate-key
 # ---
 # To Sign an imported key with an utltimately
 # trusted key :
+# 
 # https://raymii.org/s/articles/GPG_noninteractive_batch_sign_trust_and_send_gnupg_keys.html
-# --- 
+# ---
 # Now silently ultimately trusting all PGP Keys
 for fpr in $(gpg --list-keys --with-colons  | awk -F: '/fpr:/ {print $10}' | sort -u); do  echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key $fpr trust; done
 
