e6e4d56c5b08a3f0606afb92665af3d4f181cd1d
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-23 06:35:58 +0100

    Resuming work on [k8s-provisioning]

--- oci/kubeone_operator/install-terraform.sh
+++ oci/kubeone_operator/install-terraform.sh
@@ -6,20 +6,20 @@
 # I privately mirror it on git@gitlab.com:second-bureau/bellerophon/terraform/terraform.git
 # You should also reference your own private mirror of this repo, in your automation recipes
 # because then you will remove any external service build time and runtime dependency from your CI/CD factory, your pipelines run faster, with more reliability.
-export TERRAFORM_VERSION=0.11.0
+export TERRAFORM_VERSION=${TERRAFORM_VERSION:-'0.12.24'}
 # ---
 # can be :
 # => 'darwin' (mac os)
 # => 'linux' (for any GNU/Linux distrib.)
 # Terraform does not support Windows
 #
-export TERRAFORM_OS=linux
+export TERRAFORM_OS=${TERRAFORM_OS:-'linux'}
 # ---
 # can be :
 # => 'amd64' (mac os)
 # Terraform does not support any other CPU ARCH to my knowledge
 #
-export TERRAFORM_CPU_ARCH=amd64
+export TERRAFORM_CPU_ARCH=${TERRAFORM_CPU_ARCH:-'amd64'}
 
 
 export TERRAFORM_PKG_DWLD_URI="https://github.com/hashicorp/terraform/archive/terraform_${TERRAFORM_VERSION}_${TERRAFORM_OS}_${TERRAFORM_CPU_ARCH}.zip"
@@ -27,17 +27,41 @@ export TERRAFORM_PKG_DWLD_URI="https://releases.hashicorp.com/terraform/${TERRAF
 
 export TERRAFORM_CHECKSUMS_FILE_DWLD_URI="https://releases.hashicorp.com/terraform/0.12.24/terraform_${TERRAFORM_VERSION}_SHA256SUMS"
 # Signature of the TERRAFORM_CHECKSUMS_FILE_DWLD_URI, to verify the signature of the checksum file.
-export TERRAFORM_CHECKSUMS_FILE_SIGATURE_DWLD_URI="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig"
-# HASHICORP_PGP_SIGNING_KEY => to verify [TERRAFORM_CHECKSUMS_FILE_SIGATURE_DWLD_URI]
+export TERRAFORM_CHECKSUMS_FILE_SIGNATURE_DWLD_URI="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig"
+# HASHICORP_PGP_SIGNING_KEY => to verify [TERRAFORM_CHECKSUMS_FILE_SIGNATURE_DWLD_URI]
 # see https://www.hashicorp.com/security.html#secure-communications to find again the key its fingerprint and doc on how to automate retreiving key.
 export HASHICORP_PGP_SIGNING_KEY=./hashicorp.pgp.key
-curl -LO curl https://keybase.io/hashicorp/pgp_keys.asc -o $HASHICORP_PGP_SIGNING_KEY
+curl -LO curl https://keybase.io/hashicorp/pgp_keys.asc -o ${HASHICORP_PGP_SIGNING_KEY}
 # curl https://keybase.io/hashicorp/pgp_keys.asc | gpg --import
-gpg --with-fingerprint ${HASHICORP_PGP_SIGNING_KEY}
+gpg import ${HASHICORP_PGP_SIGNING_KEY}
+# I need a PGP Key for this container, to
+# sign HashiCorp's Key and make it a "trusted" key in
+# the eyes of [gpg]
+gpg --list-keys
+# Non, il faut générer une seule et unique fois une seule clef GPG
 
-curl -L0 https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_SHA256SUMS.sig
+# TODO : générer une clef ?
+gpg --full-generate-key
 
+# ---
+# To Sign an imported key with an utltimately
+# trusted key :
+# https://raymii.org/s/articles/GPG_noninteractive_batch_sign_trust_and_send_gnupg_keys.html
+# --- 
+# Now silently ultimately trusting all PGP Keys
+for fpr in $(gpg --list-keys --with-colons  | awk -F: '/fpr:/ {print $10}' | sort -u); do  echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key $fpr trust; done
 
+gpg --verify ./terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig ./terraform_${TERRAFORM_VERSION}_SHA256SUMS
+
+if [ "$?" == "0" ]; then
+  echo "Successfully checked trusted HashiCorp signature of the downloaded checksum file [./terraform_${TERRAFORM_VERSION}_SHA256SUMS]"
+  echo "Proceeding installation"
+else
+  echo "HashiCorp signature check  of the downloaded checksum file [./terraform_${TERRAFORM_VERSION}_SHA256SUMS] failed."
+  echo "check yourself the Hashicorp signature running the following command : "
+  echo "   gpg --verify $(pwd)/terraform_${TERRAFORM_VERSION}_SHA256SUMS.sig $(pwd)/terraform_${TERRAFORM_VERSION}_SHA256SUMS"
+  exit 3
+fi;
 
 checkIntegrityUsingTerraformChecksums () {
   curl -LO "${TERRAFORM_CHECKSUMS_FILE_DWLD_URI}"
@@ -55,7 +79,7 @@ checkIntegrityUsingTerraformChecksums () {
     echo "Integrity check failed for the downloaded terraform version ${TERRAFORM_VERSION} package for ${TERRAFORM_OS} OS on ${TERRAFORM_CPU_ARCH} cpu"
     echo "check yourself the integrity breach running the following command : "
     echo "   zip -T $(pwd)/terraform_${TERRAFORM_VERSION}_${TERRAFORM_OS}_${TERRAFORM_CPU_ARCH}.zip"
-    exit 3
+    exit 5
   fi;
 }
 
