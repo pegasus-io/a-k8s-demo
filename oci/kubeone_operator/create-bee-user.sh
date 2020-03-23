603784f3ae3db969b64c8b3ce237a29664561edf
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-23 02:00:36 +0100

    Resuming work on [k8s-provisioning]

--- oci/kubeone_operator/create-bee-user.sh
+++ oci/kubeone_operator/create-bee-user.sh
@@ -1,7 +1,18 @@
 #/bin/bash
 
+if [ "$(whoami)" == "root" ]; then
+  echo "running [$0] as root"
+else
+  echo "you must run [$0] as root"
+  exit 2
+fi;
+
 useradd ${BUMBLEBEE_LX_USERNAME}
-echo "implement that, and run it with bash on alpine"
+
+echo ''
+echo "implementing that, still running on dev mode "
+echo ''
 
 # https://github.com/mhart/alpine-node/issues/48#issuecomment-370171836
-exit 127
+# https://gitlab.com/second-bureau/pegasus/pegasus/-/issues/117
+exit 99
