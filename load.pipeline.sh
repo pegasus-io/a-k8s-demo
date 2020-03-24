7171c7f9d5017a810c5f9423ce1df87c7d50b1d2
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-24 20:09:01 +0100

    ajout de la provision robot

--- load.pipeline.sh
+++ load.pipeline.sh
@@ -8,13 +8,14 @@ export OPS_HOME=$(pwd)
 source .little.pipeline.env
 
 ./create-bumblebee.sh
-echo ''
-echo 'POINT DEBUG'
-exit 0
+
 source .little.pipeline.env
 # clean
 docker-compose down --rmi all && docker system prune -f --all && docker system prune -f --volumes
 # build
 docker-compose -f docker-compose.build.yml build kubeone_operator
+echo ''
+echo 'POINT DEBUG'
+exit 0
 # run
 docker-compose up -d --force-recreate kubeone_gitops_operator && docker-compose logs -f kubeone_gitops_operator
