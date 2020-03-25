800c0cb4f8d5adebd201bf090d67dafee6cae7cd
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-25 01:17:44 +0100

    ajout de la provision robot

--- oci/kubeone_operator/run.sh
+++ oci/kubeone_operator/run.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-
+# -----------------------------------------------------#
+# [ - This script uses error codes range - [10/19] - ]
+# -----------------------------------------------------#
 # set -e
 set -x
 
 echo "Entrée dans [$0]"
 cd $BUMBLEBEE_HOME_INSIDE_CONTAINER
-./init-iaac.sh || exit 4
-./init-aws.sh || exit 5
-./kubeone-prepare.sh || exit 3
+./init-iaac.sh || exit 11
+./init-aws.sh || exit 12
+# ---------------------------------------------------------
+# Now installing TERRAFORM clean syntax module utility
+# ---------------------------------------------------------
+# creates a terraform linux user group
+./install-terraform-clean-syntax.sh || exit 13
+./kubeone-prepare.sh || exit 14
 # installations are done at OCI image build time
-# ./install-kubeone.sh || exit 6
-# ./install-terraform.sh || exit 7
+# ./install-kubeone.sh || exit 15
+# ./install-terraform.sh || exit 16
