33b4bba6060734390968bfeb4c4fee7fca163f24
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-26 08:25:06 +0100

    Recette installation de minikube sans VM

--- oci/kubeone_operator/terraform.minikube/output.tf
+++ oci/kubeone_operator/terraform.minikube/output.tf
@@ -1,3 +1,9 @@
 output "vm_id" {
     value = "${aws_instance.creshVM.id}"
 }
+output "ssh_finger_print" {
+    value = "${aws_key_pair.fingerprint}"
+}
+output "ssh_pub_key" {
+    value = "${aws_key_pair.fingerprint}"
+}
