75acb343b582d0fe8623f0cd4665c145cf811de6
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-26 02:17:45 +0100

    Recette installation de minikube sans VM

--- oci/kubeone_operator/ansible.minikube/install-kubectl.sh
+++ oci/kubeone_operator/ansible.minikube/install-kubectl.sh
@@ -8,23 +8,20 @@ set -x
 # check that there is no need for virtualizaion to run
 # minikube with the '--vm-driver=none' option
 echo '---------------------------------------------------------------------------------'
-echo '---   VIRUTALIZATION CAPABILITIES OF CONTAINERIZATION HOST [$(hostname)] :'
+echo '---   Install kubectl on host [$(hostname)] :'
 echo '---------------------------------------------------------------------------------'
-grep -E --color 'vmx|svm' /proc/cpuinfo
-echo '---------------------------------------------------------------------------------'
-echo " Has [$(hostname)] virtuamlization capabilities ?"
-egrep -q 'vmx|svm' /proc/cpuinfo && echo yes || echo no
-echo '---------------------------------------------------------------------------------'
-
 
 export KUBECTL_VERSION_TAG=latest
 # https://github.com/kubernetes/minikube/releases/tag/v1.8.2
 export KUBECTL_VERSION=${KUBECTL_VERSION:-'1.18.0'}
+export KUBECTL_OS=${KUBECTL_OS:-'linux'}
+export KUBECTL_CPU_ARCH=${KUBECTL_CPU_ARCH:-'amd64'}
+
 export KUBECTL_VERSION_TAG="v${KUBECTL_VERSION}"
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/amd64/kubectl \
+curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION_TAG}/bin/${KUBECTL_OS}/${KUBECTL_CPU_ARCH}/kubectl \
   && chmod +x kubectl
 
 # alsofor the minikube binary to be there :
-sudo mv ./minikube /usr/local/bin
-minikube version
+sudo mv ./kubectl /usr/local/bin
+kubectl version
