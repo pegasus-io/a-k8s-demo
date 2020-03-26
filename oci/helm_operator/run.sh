466022650127df94bb65ef733f41405b00c229aa
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-26 06:25:45 +0100

    Recette installation de minikube sans VM

--- oci/helm_operator/run.sh
+++ oci/helm_operator/run.sh
@@ -9,5 +9,7 @@ set -x
 echo "Entrée dans [$0]"
 cd $BUMBLEBEE_HOME_INSIDE_CONTAINER
 ./init-iaac.sh || exit 11
-./init-helm.sh || exit 12
+./install-kubectl.sh || exit 12
+./install-helm.sh || exit 12
+
 exec "${SHELL}"
