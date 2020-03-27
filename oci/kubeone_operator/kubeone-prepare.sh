d4810d2d1cb93023ca76a3a18b511db13561249f
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-27 03:26:12 +0100

    Correction des erreurs de la release 0.0.2

--- oci/kubeone_operator/kubeone-prepare.sh
+++ oci/kubeone_operator/kubeone-prepare.sh
@@ -169,7 +169,7 @@ echo "---  # [outside container] : "
 echo "---  "
 echo "---  ssh -i ${BUMBLEBEE_SECRETS_VAULT_OUTSIDE_CONTAINERS}/.aws/aws.creshkey.pem ec2-user@${PUBLIC_EIP_OF_AWS_INSTANCE}"
 echo "---  "
-sudo ping -c 4 ${PUBLIC_EIP_OF_AWS_INSTANCE}
-sudo ping4 -c 4 ${PUBLIC_EIP_OF_AWS_INSTANCE}
+# sudo ping -c 4 ${PUBLIC_EIP_OF_AWS_INSTANCE}
+# sudo ping4 -c 4 ${PUBLIC_EIP_OF_AWS_INSTANCE}
 echo '------------------------------------------------------------------------'
 # ssh -Tvai ${BUMBLEBEE_HOME_INSIDE_CONTAINER}/.secrets/.aws/aws.creshkey.pem ec2-user@${PUBLIC_EIP_OF_AWS_INSTANCE}
