a59fc75c46caeb37446a9db5da91fe078aa0a4b5
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-27 08:59:41 +0100

    Correction des erreurs de la release 0.0.2

--- oci/aws-cli/v2/healthcheck.sh
+++ oci/aws-cli/v2/healthcheck.sh
@@ -12,13 +12,19 @@ export FILE_TO_CHECK=/home/${BUMBLEBEE_USER}/creshAWSSSHkey
 
 
 checkWitnessFile () {
-
+  if [ -f $1 ]; then
+    echo "The file [$1] was found, health changes to healthy:  EC2 KeyPair is READY"
+    return 0
+  else
+    echo "The file [$1] was NOT found, health changes to healthy:  EC2 KeyPair is NOT READY"
+    return 1
+  fi;
 }
 
-if [ -f ${FILE_TO_CHECK} ]; then
-  echo "The file [${FILE_TO_CHECK}] was found, health changes to healthy:  EC2 KeyPair is ready"
-  exit 0
-else
-  sleep 1
-  checkWitnessFile
-fi;
+export IS_EC2_PAIR_READY=1
+while [ "$IS_EC2_PAIR_READY" != "0" ]
+do
+  echo "checking health of the [AWSCLI_OPERATOR] container ..."
+  export IS_EC2_PAIR_READY=$(checkWitnessFile $FILE_TO_CHECK)
+  sleep 1s
+done
