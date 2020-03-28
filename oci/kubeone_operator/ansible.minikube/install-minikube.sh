65989c80ef455b90e7d2fcfd6a4b04f08973018e
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-28 10:48:03 +0100

    corrections sur le AWSCLI operator responable de créer le AWS EC2 KeyPair

--- oci/kubeone_operator/ansible.minikube/install-minikube.sh
+++ oci/kubeone_operator/ansible.minikube/install-minikube.sh
@@ -40,10 +40,11 @@ sudo mv ./minikube /usr/bin
 # export THIS_RECIPES_RELEASE=feature/aws-provisioning
 # git clone "$URI_DE_CE_REPO" .
 # git checkout $THIS_RECIPES_RELEASE
-chmod +x ./oci/kubeone_operator/ansible.minikube/*.sh
+chmod +x ./oci/kubeone_operator/ansible.minikube/kubectl/*.sh
 ./oci/kubeone_operator/ansible.minikube/kubectl/install-kubectl-on-minikube.sh
 
 minikube version
+
 # --- #
 # sets the none driver as the default : we don't have to use the '--vm-driver' option again.
 # --- #
