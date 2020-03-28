c109b38e6577b0b67299abc6ca17f10eccd455b4
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-28 14:57:13 +0100

    corrections sur le AWSCLI operator responable de créer le AWS EC2 KeyPair

--- oci/kubeone_operator/ansible.minikube/prepare-amazon-linux-2.sh
+++ oci/kubeone_operator/ansible.minikube/prepare-amazon-linux-2.sh
@@ -2,12 +2,8 @@
 
 set -x
 
-# The Amazon Linux 2 is just a RHEL / CentOS derivative. 
+# The Amazon Linux 2 is just a RHEL / CentOS derivative.
 # So I need Git, and docker in my AWS image, before running minikube install
-
-sudo yum update -y
-#
-sudo yum install -y git
 
 echo '------------------------------------------------------------'
 echo ''
@@ -15,6 +11,9 @@ cat /etc/os-release
 echo ''
 echo '------------------------------------------------------------'
 
+sudo yum update -y
+
+# ---
+# - git : to operate as a gitops
 # - docker installation : this install is VERY bad, it does not install a specific verson of docker, just latest, so time dependent
-# curl -fsSL https://get.docker.com/ | sh # => Amazon Linux is not supported by Docker
-sudo yum install -y docker
+sudo yum install -y git docker
