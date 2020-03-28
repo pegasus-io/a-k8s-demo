73322b838ff488fbd84991b80eea65a24926a599
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-28 11:36:11 +0100

    corrections sur le AWSCLI operator responable de créer le AWS EC2 KeyPair

--- oci/kubeone_operator/ansible.minikube/prepare-aws-linux-ami.sh
+++ oci/kubeone_operator/ansible.minikube/prepare-aws-linux-ami.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 set -x
+
+# So I need Git and docker installed, before running minikube install
+
 sudo yum update -y
-sudo yum install git systemctl
+#
+sudo yum install git
 
 echo '------------------------------------------------------------'
 echo ''
 cat /etc/os-release
 echo ''
 echo '------------------------------------------------------------'
+
+# - docker installation
+curl -fsSL https://get.docker.com/ | sh
