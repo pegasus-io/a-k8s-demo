9e3082912fcb2632e73c17ac24cc717878394ddb
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-30 00:19:47 +0200

    travail sur le build de l'image oci helm operator

--- oci/aws-cli/v2/retrieve-ami-id.sh
+++ oci/aws-cli/v2/retrieve-ami-id.sh
@@ -2,12 +2,13 @@
 
 # ------
 #
-# This script retieves from AWS the
+# This script retieves from AWS the current Amazon Linux 2 AMI ID
+#
+# just as recommended by https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/finding-an-ami.html#finding-an-ami-console
 #
 # ------
 
 set +x
-
 
 aws ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.????????.?-x86_64-gp2' 'Name=state,Values=available' --query 'reverse(sort_by(Images, &CreationDate))[:1].ImageId' --output text > ./amazon.linux.ami.id
 
