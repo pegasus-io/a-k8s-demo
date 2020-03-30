961a99fcc3d2cda5ea978f82a1abefae3a26b5c0
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-03-30 03:22:43 +0200

    travail sur le build de l'image oci helm operator travail sur le build de l'image oci helm operator

--- oci/helm_operator/run.sh
+++ oci/helm_operator/run.sh
@@ -8,10 +8,12 @@ set -x
 
 echo "Entrée dans [$0]"
 
-${BUMBLEBEE_HOME_INSIDE_CONTAINER}/install-git-flow.sh || exit 10
-${BUMBLEBEE_HOME_INSIDE_CONTAINER}/init-iaac.sh || exit 11
+# ${BUMBLEBEE_HOME_INSIDE_CONTAINER}/install-git-flow.sh || exit 10
 # ./install-kubectl.sh || exit 12
 # ./install-helm.sh || exit 12
+${BUMBLEBEE_HOME_INSIDE_CONTAINER}/init-iaac.sh || exit 11
 ${BUMBLEBEE_HOME_INSIDE_CONTAINER}/configure-kubectl.sh || exit 12
+
+
 
 exec "${SHELL}"
